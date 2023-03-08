a284aeaa4a702424c60512a35cc39619a8823040
Daniel Felipe Caicedo Trujillo <daniel-caicedo@unipiloto.edu.co>
2023-03-08 17:54:48 -0500

    ejemplo de una clase persona

---
+++ ejemplo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el nombre de la persona");
+            persona perso;
+            string a = Console.ReadLine();
+            perso = new persona(a.ToUpper(), 45, 80);
+            perso.comer(150);
+            perso.Caminar(10);
+            perso.comer(1500);
+            perso.MontarBici(100);
+            perso.finalizardia();
+            Console.WriteLine(perso.getPeso());
+            perso.despertar();
+            perso.comer(150);
+            perso.Caminar(10);
+            perso.comer(2900);
+            perso.MontarBici(100);
+            perso.finalizardia();
+            perso.dormir();
+            perso.MontarBici(1500);
+            perso.finalizardia();
+            Console.WriteLine(perso.getPeso());
+        }
+    }
+}
---
+++ ejemplo/persona.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ejemplo
+{
+    class persona
+    {
+        int edad,  peso;
+        string nombre; //Atributos
+        int cant_calorias;
+        bool despierto;
+        public persona(string nombre, int edad, int peso) // Constructor -> Metodo
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.peso = peso;
+            despierto = false;
+            cant_calorias = 0;
+        }
+        public void despertar()
+        {
+            despierto = true;
+        }
+        public void dormir()
+        {
+            despierto = false;
+        }
+
+        public void AdicionarComida(int calorias)
+        {
+            if (despierto)
+                cant_calorias += calorias;
+        }
+        public void consumircalorias(int calorias)
+        {
+            if (despierto)
+                cant_calorias -= calorias;
+        }
+        public void MontarBici(int kms)
+        {
+            consumircalorias(20 * kms);
+        }
+        public void Caminar(int kms)
+        {
+            consumircalorias(50 * kms);
+        }
+        public void comer(int calorias)
+        {
+            this.AdicionarComida(calorias);
+        }
+        public void finalizardia()
+        {
+            this.peso += (cant_calorias) / 500;
+            cant_calorias = 0;
+        }
+        public int getPeso()
+        {
+            return this.peso;
+        }
+        
+
+
+    }
+}
